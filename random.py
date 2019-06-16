3b8e86e062c207b8436da9f00829f56d44727bfa
bhargava <ranga.bhargava95@gmail.com>
2019-06-15 19:15:31 -0700

    New

---
+++ random.py
@@ -0,0 +1,26 @@
+import json
+import os
+from faker import Faker
+fake = Faker()
+
+
+w = {}
+a = []
+for i in range(10):
+    w['userIdentifier'] = i
+    if i in d.keys():
+        w['country'] = d[i]
+    w['devOrTestUser'] = str(fake.boolean(chance_of_getting_true=10)).lower()
+    w['firstName'] = fake.first_name()
+    w['middleInitial'] = ''
+    w['lastName'] = fake.last_name()
+    w['nameSuffix'] = ''
+    w['email'] = fake.email()
+    w['dateOfBirth'] = str(fake.date_between(start_date='-48y', end_date='-18y'))
+    a.append(w.copy())
+
+
+e = {"users": a}
+print(e)
+with open(os.path.join(r"C:\Users\User1\Desktop\Test Data\Output", 'flash_reports.json'), 'w') as outfile:
+    json.dump(e, outfile, indent=2)
